f387fec342d975bd7495e2273669650733886e58
pallab2006 <pallabsaikia784@gmail.com>
2024-11-18 15:35:15 +0530

    Add files via upload

---
+++ JAVASCRIPT/first.js
@@ -0,0 +1,12 @@
+let score= 7;
+if(score>=80 && score<=100){
+    console.log("GRADE: A")
+} else if(score>=70 && score<= 89){
+    console.log("GRADE:B")
+}else if(score>=60 && score<= 69){
+    console.log("GRADE:C")
+}else if(score>=50 && score<= 59){
+    console.log("GRADE:D")
+} else{
+    console.log("grade:F")
+}
---
+++ JAVASCRIPT/pallab.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("hello oeld");
+    return 0;
+};
